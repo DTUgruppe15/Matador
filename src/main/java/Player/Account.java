977836c4cdaf30afd96318eae467d78e5050c728
valde12 <s224289@DTU.dk>
2023-01-05 14:49:37 +0100

    added javadoc comments to all methods in player and account

--- src/main/java/Player/Account.java
+++ src/main/java/Player/Account.java
@@ -8,6 +8,9 @@ public class Account {
     private ArrayList<Deed> mortgagedDeeds;
     private int totalValue;
 
+    /**
+     * Constructor for the Account class
+     */
     public Account() {
         this.balance = 30000;
         this.ownedDeeds = new ArrayList<Deed>();
@@ -15,6 +18,13 @@ public class Account {
         this.totalValue = balance;
     }
 
+    /**
+     * Adds a Deed object to the list of owned Deeds and removes it from the list of mortgaged Deeds if it is present.
+     * If the Deed is not in the list of mortgaged Deeds, it is simply added to the list of owned Deeds.
+     * Updates the total value of the Account.
+     *
+     * @param newDeed the Deed object to be added to the list of owned Deeds
+     */
     public void updateOwnedDeeds(Deed newDeed) {
         if (this.mortgagedDeeds.contains(newDeed)) {
             this.ownedDeeds.add(newDeed);
@@ -25,6 +35,13 @@ public class Account {
         updateTotalValue();
     }
 
+    /**
+     * Adds a Deed object to the list of mortgaged Deeds and removes it from the list of owned Deeds if it is present.
+     * If the Deed is not in the list of owned Deeds, it is simply added to the list of mortgaged Deeds.
+     * Updates the total value of the Account.
+     *
+     * @param newDeed the Deed object to be added to the list of mortgaged Deeds
+     */
     public void updateMortgagedDeeds(Deed newDeed) {
         if (this.ownedDeeds.contains(newDeed)) {
             this.mortgagedDeeds.add(newDeed);
@@ -35,25 +52,43 @@ public class Account {
         updateTotalValue();
     }
 
-    private void updateTotalValue(){
+    /**
+     * Updates the total value of the Account by adding the value of all owned Deed objects to the balance.
+     */
+    private void updateTotalValue() {
         int valueOfOwnedDeeds = 0;
-        for(Deed ownedDeed : ownedDeeds) {
+        for (Deed ownedDeed : ownedDeeds) {
             ownedDeed.getValue;
         }
-        totalValue = balance+valueOfOwnedDeeds;
+        totalValue = balance + valueOfOwnedDeeds;
     }
 
+    /**
+     * Returns the balance of the Account.
+     *
+     * @return the balance of the Account
+     */
     public int getBalance() {
         return balance;
     }
 
+    /**
+     * Sets the balance of the Account.
+     *
+     * @param balance the new balance to be set for the Account
+     */
     public void setBalance(int balance) {
         this.balance = balance;
         updateTotalValue();
     }
 
+    /**
+     * Updates the balance of the Account by adding the specified amount to the current balance.
+     *
+     * @param amount the amount to be added to the balance
+     */
     public void updateBalance(int amount) {
-        balance = balance+amount;
+        balance = balance + amount;
         updateTotalValue();
     }
 }
