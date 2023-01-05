0f0329927e03c1dcc9af5fe372ca596f412e6b1e
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2023-01-05 10:08:53 +0100

    account and player setup, work in progress

--- src/main/java/Player/Account.java
+++ src/main/java/Player/Account.java
@@ -1,16 +1,34 @@
 package Player;
 
+import java.util.ArrayList;
+
 public class Account {
     private int balance;
-    private ownedDeeds;
-    private Deed[] mortgagedDeeds;
+    private ArrayList<Deed> ownedDeeds;
+    private ArrayList<Deed> mortgagedDeeds;
     private int totalValue;
 
     public Account() {
         this.balance = 30000;
-        this.ownedDeeds;
-        this.mortgagedDeeds;
+        this.ownedDeeds = new ArrayList<Deed>();
+        this.mortgagedDeeds = new ArrayList<Deed>();
         this.totalValue = balance;
+    }
+
+    public void UpdateOwnedDeeds(Deed newDeed) {
+        boolean isMortgaged = false;
+        int mortgagedDeedPosition = 0;
+        for(Deed checkDeed : mortgagedDeeds) {
+            if (checkDeed.getLocation = newDeed.getLocation) {
+                isMortgaged = true;
+
+                break;
+            }
+        }
+        if (isMortgaged) {
+            ownedDeeds.add(Deed);
+            mortgagedDeeds.remove(Deed);
+        }
     }
 
     public int getBalance() {
