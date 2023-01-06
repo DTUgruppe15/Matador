3e744d9a1f3e90da4869c11d0fe75df9d154394d
Magnus Ottosen <magnus@ottosen.dk>
2023-01-06 14:56:50 +0100

    GUI made

---
+++ src/main/java/CSVReader.java
@@ -0,0 +1,132 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+    private String[] name = new String[40];
+    private int[] position = new int[40];
+    private String[] type = new String[40];
+    private int[] price = new int[40];
+    private int[] housePrice = new int[40];
+    private int[] rent0 = new int[40];
+    private int[] rent1 = new int[40];
+    private int[] rent2 = new int[40];
+    private int[] rent3 = new int[40];
+    private int[] rent4 = new int[40];
+    private int[] rent5 = new int[40];
+
+    public String getName(int index){
+        return name[index];
+    }
+
+    public int getPosition(int index){
+        return position[index];
+    }
+
+    public String getType(int index){
+        return type[index];
+    }
+
+    public int getPrice(int index){
+        return price[index];
+    }
+
+    public int getHousePrice(int index){
+        return housePrice[index];
+    }
+
+    public int getRent0(int index){
+        return rent0[index];
+    }
+
+    public int getRent1(int index){
+        return rent1[index];
+    }
+
+    public int getRent2(int index){
+        return rent2[index];
+    }
+
+    public int getRent3(int index){
+        return rent3[index];
+    }
+
+    public int getRent4(int index){
+        return rent4[index];
+    }
+
+    public int getRent5(int index){
+        return rent5[index];
+    }
+
+    public void readFile(){
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("src\\main\\resources\\fields.csv"));
+
+            String line = "";
+            String[] tempArr;
+            int index = 0;
+
+            bufferedReader.readLine();
+            while((line = bufferedReader.readLine()) != null){
+                tempArr = line.split(",",-1);
+
+                for (int i = 0; i < tempArr.length; i++) {
+                    if(tempArr[i] == ""){
+                        if(i == 0 || i == 2){
+                            tempArr[i] = "N/A";
+                        }else{
+                            tempArr[i] = "0";
+                        }
+                    }
+                }
+
+
+                name[index] = tempArr[0];
+                position[index] = Integer.parseInt(tempArr[1]);
+                type[index] = tempArr[2];
+                price[index] = Integer.parseInt(tempArr[3]);
+                housePrice[index] = Integer.parseInt(tempArr[4]);
+                rent0[index] = Integer.parseInt(tempArr[5]);
+                rent1[index] = Integer.parseInt(tempArr[6]);
+                rent2[index] = Integer.parseInt(tempArr[7]);
+                rent3[index] = Integer.parseInt(tempArr[8]);
+                rent4[index] = Integer.parseInt(tempArr[9]);
+                rent5[index] = Integer.parseInt(tempArr[10]);
+
+                index++;
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public CSVReader(){
+        readFile();
+    }
+
+    public static void main(String[] args) {
+        CSVReader temp = new CSVReader();
+
+        temp.readFile();
+
+        for (int i = 0; i < 40; i++) {
+            System.out.println(temp.getType(i));
+        }
+
+
+
+    }
+
+
+
+
+}
--- src/main/java/UpdateGUI.java
+++ src/main/java/UpdateGUI.java
@@ -1,6 +1,4 @@
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Player;
-import gui_fields.GUI_Street;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -21,11 +19,126 @@ public class UpdateGUI {
     }
 
     public void initBoard(){
+
+        CSVReader csvReader = new CSVReader();
+
         fields = new GUI_Field[40];
         for (int i = 0; i < fields.length; i++) {
-            fields[i] = new GUI_Street();
+            System.out.println(csvReader.getType(i));
+            switch (csvReader.getType(i)){
+                case " start":
+                    fields[i] = new GUI_Start();
+                    fields[i].setTitle(csvReader.getName(i));
+                    fields[i].setSubText("<----");
+                    break;
+                case " street":
+                    fields[i] = new GUI_Street();
+                    fields[i].setTitle(csvReader.getName(i));
+                    fields[i].setSubText(Integer.toString(csvReader.getPrice(i)));
+                    break;
+                case " chance":
+                    fields[i] = new GUI_Chance();
+                    break;
+                case " tax":
+                    fields[i] = new GUI_Tax();
+                    fields[i].setTitle(csvReader.getName(i));
+                    fields[i].setSubText(Integer.toString(csvReader.getPrice(i)));
+                    break;
+                case " ferry":
+                    fields[i] = new GUI_Shipping();
+                    fields[i].setTitle(csvReader.getName(i));
+                    fields[i].setSubText(Integer.toString(csvReader.getPrice(i)));
+                    break;
+                case " jail":
+                    fields[i] = new GUI_Jail();
+                    if(csvReader.getPosition(i) == 10){
+                        fields[i].setSubText("På besøg");
+                    }else{
+                        fields[i].setSubText("Gå i fængsel");
+                    }
+                    break;
+                case " brewery":
+                    fields[i] = new GUI_Brewery();
+                    fields[i].setTitle(csvReader.getName(i));
+                    fields[i].setSubText(Integer.toString(csvReader.getPrice(i)));
+                    break;
+                case " refugee":
+                    fields[i] = new GUI_Refuge();
+                    fields[i].setSubText("Parkering");
+                    break;
+                default:
+                    fields[i] = new GUI_Street();
+                    fields[i].setBackGroundColor(Color.RED);
+                    break;
+            }
         }
+
+        fields[0].setBackGroundColor(Color.RED);
+        fields[1].setBackGroundColor(Color.BLUE);
+        fields[2].setBackGroundColor(Color.BLACK);
+        fields[2].setForeGroundColor(Color.GREEN);
+        fields[3].setBackGroundColor(Color.BLUE);
+        fields[4].setBackGroundColor(Color.CYAN);
+        fields[5].setBackGroundColor(Color.BLUE);
+        fields[6].setBackGroundColor(Color.ORANGE);
+        fields[7].setBackGroundColor(Color.BLACK);
+        fields[7].setForeGroundColor(Color.GREEN);
+        fields[8].setBackGroundColor(Color.ORANGE);
+        fields[9].setBackGroundColor(Color.ORANGE);
+        fields[10].setBackGroundColor(Color.BLACK);
+        fields[10].setForeGroundColor(Color.WHITE);
+        fields[11].setBackGroundColor(Color.YELLOW);
+        fields[12].setBackGroundColor(Color.RED);
+        fields[13].setBackGroundColor(Color.YELLOW);
+        fields[14].setBackGroundColor(Color.YELLOW);
+        fields[15].setBackGroundColor(Color.RED);
+        fields[16].setBackGroundColor(Color.GRAY);
+        fields[17].setBackGroundColor(Color.BLACK);
+        fields[17].setForeGroundColor(Color.GREEN);
+        fields[18].setBackGroundColor(Color.GRAY);
+        fields[19].setBackGroundColor(Color.GRAY);
+        fields[20].setBackGroundColor(Color.CYAN);
+        fields[21].setBackGroundColor(Color.RED);
+        fields[22].setBackGroundColor(Color.BLACK);
+        fields[22].setForeGroundColor(Color.GREEN);
+        fields[23].setBackGroundColor(Color.RED);
+        fields[24].setBackGroundColor(Color.RED);
+        fields[25].setBackGroundColor(Color.BLUE);
+        fields[26].setBackGroundColor(Color.LIGHT_GRAY);
+        fields[27].setBackGroundColor(Color.LIGHT_GRAY);
+        fields[28].setBackGroundColor(Color.RED);
+        fields[29].setBackGroundColor(Color.LIGHT_GRAY);
+        fields[30].setBackGroundColor(Color.BLACK);
+        fields[30].setForeGroundColor(Color.WHITE);
+        fields[31].setBackGroundColor(Color.YELLOW);
+        fields[32].setBackGroundColor(Color.YELLOW);
+        fields[33].setBackGroundColor(Color.BLACK);
+        fields[33].setForeGroundColor(Color.GREEN);
+        fields[34].setBackGroundColor(Color.YELLOW);
+        fields[35].setBackGroundColor(Color.BLUE);
+        fields[36].setBackGroundColor(Color.BLACK);
+        fields[36].setForeGroundColor(Color.GREEN);
+        fields[37].setBackGroundColor(Color.MAGENTA);
+        fields[38].setBackGroundColor(Color.CYAN);
+        fields[39].setBackGroundColor(Color.MAGENTA);
+
+
+
     }
+
+    public void sendMessage(String text){
+        gui.showMessage(text);
+    }
+
+    public void setBalance(int player, int money){
+        players[player].setBalance(money);
+    }
+
+    public void setDice(int die1, int die2){
+        gui.setDice(die1, die2);
+    }
+
+
 
 
 
