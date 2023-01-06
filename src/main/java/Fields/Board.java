02a2902f40e78e0822f675178dbe3b26701deab8
S185439 <113907421+S185439@users.noreply.github.com>
2023-01-06 12:26:22 +0100

    Game kan nu kalde felterne

--- src/main/java/Fields/Board.java
+++ src/main/java/Fields/Board.java
@@ -8,8 +8,9 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Board {
-    Fields[] fields = new Fields[40];
-    public void createFields(String fileName) {
+
+    public Fields[] createFields(Fields[] fields) {
+        Fields[] fields1 = fields;
         BufferedReader br;
         String line = "";
         int whatField = -1;
@@ -20,19 +21,16 @@ public class Board {
                 System.out.println(fieldValues[2]);
                 System.out.println(whatField);
                 switch (fieldValues[2]) {
-                    case " street" -> {
-                        fields[whatField] = new Plot(fieldValues[0], Integer.parseInt(fieldValues[3]), Integer.parseInt(fieldValues[4]), Integer.parseInt(fieldValues[5]),
+                    case " street" -> fields1[whatField] = new Plot(fieldValues[0], Integer.parseInt(fieldValues[3]), Integer.parseInt(fieldValues[4]), Integer.parseInt(fieldValues[5]),
                                 Integer.parseInt(fieldValues[6]), Integer.parseInt(fieldValues[7]), Integer.parseInt(fieldValues[8]), Integer.parseInt(fieldValues[9]),
                                 Integer.parseInt(fieldValues[10]));
-                        System.out.println("yeh ");
-                    }
-                    case " brewery" -> fields[whatField] = new Brewery(fieldValues[0], Integer.parseInt(fieldValues[3]));
-                    case " ferry" -> fields[whatField] = new Ferry(fieldValues[0], Integer.parseInt(fieldValues[3]));
-                    case " start" -> fields[whatField] = new Go(fieldValues[0]);
-                    case " chance" -> fields[whatField] = new Chance();
-                    case " tax" -> fields[whatField] = new Tax(fieldValues[0], Integer.parseInt(fieldValues[3]));
-                    case " jail" -> fields[whatField] = new Jail();
-                    case " refugee" -> fields[whatField] = new Parking();
+                    case " brewery" -> fields1[whatField] = new Brewery(fieldValues[0], Integer.parseInt(fieldValues[3]));
+                    case " ferry" -> fields1[whatField] = new Ferry(fieldValues[0], Integer.parseInt(fieldValues[3]));
+                    case " start" -> fields1[whatField] = new Go(fieldValues[0]);
+                    case " chance" -> fields1[whatField] = new Chance();
+                    case " tax" -> fields1[whatField] = new Tax(fieldValues[0], Integer.parseInt(fieldValues[3]));
+                    case " jail" -> fields1[whatField] = new Jail();
+                    case " refugee" -> fields1[whatField] = new Parking();
                 }
                 whatField++;
 
@@ -42,12 +40,11 @@ public class Board {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return fields1;
     }
 
 
-    public Board() {
-        createFields("src\\main\\resources\\fields.csv");
-    }
+    public Board(){}
 
 
 
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -16,16 +16,17 @@ public class Game {
     public void run(){
         players = new Player[1];
         players[0] = new Player();
-
+        fields = new Fields[40];
         Board board = new Board();
+        fields = board.createFields(fields);
 
 
 
-        /* for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 40; i++) {
             fields[i].doStuff(players[0]);
             players[0].movePosition(1);
         }
-        */
+
 
     }
 
