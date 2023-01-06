53508f2bcb0f4b901675e55da2403d20c2d501b3
S185439 <113907421+S185439@users.noreply.github.com>
2023-01-06 09:43:26 +0100

    Taget nogle funktioner fra plot over i deed

--- src/main/java/Deed.java
+++ src/main/java/Deed.java
@@ -1,6 +1,7 @@
 public class Deed{
     private int price, housePrice, rent0, rent1, rent2, rent3, rent4, rent5;
     private String location;
+    private boolean bought = false;
 
     public Deed(String location, int price, int housePrice, int rent0, int rent1, int rent2, int rent3, int rent4, int rent5) {
         this.location = location;
@@ -17,6 +18,14 @@ public class Deed{
     public Deed(String location, int price) {
         this.location = location;
         this.price = price;
+    }
+
+    public void buyDeed() {
+        this.bought = true;
+    }
+
+    public boolean getBoughtStatus() {
+        return this.bought;
     }
 
     public String getLocation(){
--- src/main/java/Properties.java
+++ src/main/java/Properties.java
@@ -1,7 +1,5 @@
 public abstract class Properties extends Fields {
-    private Deed deed;
     private String label;
-    private int position;
 
 
     public Properties(String label) {
