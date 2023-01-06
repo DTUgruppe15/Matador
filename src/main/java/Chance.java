d75515cf5808b243351b6760a6ef92ae617dd019
Magnus Ottosen <magnus@ottosen.dk>
2023-01-06 11:06:33 +0100

    Update Chance.java

--- src/main/java/Chance.java
+++ src/main/java/Chance.java
@@ -1,7 +1,39 @@
+import java.util.Random;
+
 public class Chance extends Fields{
+
+    private int[] cards;
+
+    private int cardToDraw;
+
+    private int JailCardRemaining;
+
+    public Chance(){
+        cardToDraw = 0;
+        JailCardRemaining = 2;
+        cards = new int[]{0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 11, 12, 12, 12, 13, 14, 15, 16, 16, 17, 18, 19, 20, 21, 22, 23, 23, 24, 24, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 35};
+        shuffleCards();
+    }
+
+    public void shuffleCards(){
+        Random rand = new Random();
+
+        for (int i = 0; i < cards.length; i++) {
+            int indexToSwap = rand.nextInt(cards.length);
+            int temp = cards[indexToSwap];
+            cards[indexToSwap] = cards[i];
+            cards[i] = temp;
+        }
+    }
 
     @Override
     public void doStuff(Player player) {
+
+        if(cards[cardToDraw] == 35 && JailCardRemaining == 0){
+            
+        }
+
+
         System.out.println("Chance: " + getLabel());
     }
 
